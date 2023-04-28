280cd62438692d4044735e4babeebb31beb9e735
seokdaegeon <sollscherr@gmail.com>
2023-04-28 23:25:36 +0900

    줄구분 할 수 있는 키워드 txt로 추가 가능케 수정

--- pdf_text_extractor.py
+++ pdf_text_extractor.py
@@ -1,16 +1,30 @@
+import pdfplumber
 
-def read_break_word_from_file(file_path):
+def extract_text_from_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+def read_break_words_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        break_word = file.read().strip()
-    return break_word
+        break_words = [line.strip() for line in file]
+    return break_words
+
+def clean_text(text, line_break_words):
+    cleaned_text = ' '.join(text.split())
+    for word in line_break_words:
+        cleaned_text = cleaned_text.replace(word, f"\n{word}")
+    return cleaned_text
 
 if __name__ == "__main__":
     pdf_path = "/Users/seokdaegeon/PycharmProjects/sibora/seoulsibo_20230426151204_73863.pdf"  # 여기에 PDF 파일 경로를 입력하세요.
     extracted_text = extract_text_from_pdf(pdf_path)
 
     break_word_file = "break_words.txt"
-    line_break_word = read_break_word_from_file(break_word_file)
-    cleaned_text = clean_text(extracted_text, line_break_word)
+    line_break_words = read_break_words_from_file(break_word_file)
+    cleaned_text = clean_text(extracted_text, line_break_words)
 
-    truncated_text = cleaned_text[:3000]
+    truncated_text = cleaned_text[:10000]
     print(truncated_text)
